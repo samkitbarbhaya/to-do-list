fd988b9d9f9414e5d685ef2d2a7a0d15ee5c678b
Samkit Barbhaya <samkitbarbhaya@gmail.com>
2023-03-16 23:55:20 -0700

    Modified Project.JS and Task.JS

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,9 @@
 import {task} from './Task'
+import { toDate, isToday, isThisWeek, subDays } from 'date-fns'
 
 const project = (name) => {
     this.name = name
     let taskList = []
-    
     const getName = ()=> this.name
     const setName = (name)=> this.name = name
     const addTask = (taskName)=>{
@@ -12,14 +12,32 @@ const project = (name) => {
     }
     const removeTask = (taskName)=>{
         this.taskList = this.taskList.filter((taskEl)=>{
-            return taskEl.getName() != taskName
+            return taskEl.getName() !== taskName
         })
-    }   
+    }
+    const getTaskList = ()=>{
+        return taskList
+    }
+    const getTasksToday = ()=>{
+        return this.taskList.filter((taskEl)=>{
+            const taskDate = new Date(taskEl.getDate())
+            return isToday(toDate(taskDate))
+        })
+    }
+    const getTasksWeek = ()=>{
+        return this.tasks.filter((taskEl) => {
+            const taskDate = new Date(taskEl.getDate())
+            return isThisWeek(subDays(toDate(taskDate), 1))
+        })
+    }
     return {
         getName,
         setName,
         addTask,
-        removeTask
+        removeTask,
+        getTaskList,
+        getTasksToday,
+        getTasksWeek
     }
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -6,18 +6,18 @@ const task = (name) => {
     const setName = (name)=> this.name = name
     const getDate = ()=> this.dueDate
     const setDate = (dueDate)=> this.dueDate = dueDate
-    const getFormattedDate = ()=>{
-        const day = this.dueDate.split('/')[0]
-        const month = this.dueDate.split('/')[1]
-        const year = this.dueDate.split('/')[2]
-        return `${month}/${day}/${year}`
-    }
+    // const getFormattedDate = ()=>{
+    //     const day = this.dueDate.split('/')[0]
+    //     const month = this.dueDate.split('/')[1]
+    //     const year = this.dueDate.split('/')[2]
+    //     return `${month}/${day}/${year}`
+    // }
     return {
         getName,
         setName,
         getDate,
         setDate,
-        getFormattedDate
+        // getFormattedDate
     }
 }
  
